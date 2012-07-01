a794cd03347f2b9d4e6a9ff0b50479504576744c
Julia Grace <jewelia@gmail.com>
2012-07-01 02:22:50 -0700

    added in storing access code in session

--- jmz/maulia/views.py
+++ jmz/maulia/views.py
@@ -5,7 +5,6 @@ import urllib
 import urllib2
 import json
 
-singly_access_token = ''
 
 def index(request):
     return render_to_response('singly.html',
@@ -44,6 +43,8 @@ def connect_to_service(request):
 
     
     api_request_profiles = SINGLY_PROFILES + '?access_token=' + access_token
+    
+    request.session['REQ_TOKEN_SESSION_KEY'] = access_token
     
     all_api_calls = {'profile': api_request_profiles}
 
@@ -94,13 +95,15 @@ def connect_to_service(request):
                                })
 
 def singly_authorize(request):
-    SINGLY_ACCESS_TOKEN_URL = 'https://api.singly.com/oauth/access_token'
+    access_token = request.session['REQ_TOKEN_SESSION_KEY']
+    
     SINGLY_PROFILES = 'https://api.singly.com/v0/profiles'
 
     SINGLY_PHOTOS= 'https://api.singly.com/v0/types/photos'
     SINGLY_CHECKINS = 'https://api.singly.com/v0/types/checkins'
     SINGLY_STATUS = 'https://api.singly.com/v0/types/statuses'
-
+    '''
+    SINGLY_ACCESS_TOKEN_URL = 'https://api.singly.com/oauth/access_token'
     FOURSQUARE = 'https://api.singly.com/v0/services/foursquare/checkins'
 
     # Get singly access code
@@ -131,6 +134,7 @@ def singly_authorize(request):
         }
 
     get_data = urllib.urlencode(get_params)
+    '''
 
     #1 Fetch Profile Data
     api_request_profiles = SINGLY_PROFILES + '?access_token=' + access_token
