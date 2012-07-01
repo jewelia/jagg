9bc823f240609e1d44855ae2b5b424f90d7f7fe3
Julia Grace <jewelia@gmail.com>
2012-06-30 17:18:13 -0700

    Added in Twitter bootstrap and json parsing

--- jmz/maulia/views.py
+++ jmz/maulia/views.py
@@ -7,7 +7,6 @@ import urllib2
 import urlparse
 import httplib2
 import json
-import oauth2 as oauth
 from urlparse import parse_qsl, urlparse
 from urllib import urlencode
 
@@ -19,7 +18,7 @@ def singly_authorize(request):
     SINGLY_ACCESS_TOKEN_URL = 'https://api.singly.com/oauth/access_token'
     SINGLY_PROFILES = 'https://api.singly.com/v0/profiles'
 
-    SINGLY_PHOTOS= 'https://api.singly.com/v0/types/checkins'
+    SINGLY_PHOTOS= 'https://api.singly.com/v0/types/photos'
     SINGLY_CHECKINS = 'https://api.singly.com/v0/types/checkins'
     SINGLY_STATUS = 'https://api.singly.com/v0/types/statuses'
 
@@ -69,14 +68,69 @@ def singly_authorize(request):
     
     #4 Load checkins into json parser
     photo_json = json.loads(photos)
-    print photo_json
+
+    '''
+    [
+    {
+        "date": "date",
+        "source": "source",
+        "link": "link",
+        "type": "type",
+        "numb_likes": "numb_likes",
+        "image_location": "image_location",
+        "width": "width",
+        "height": "height"
+[
+"oembed": {
+  "type": "photo",
+  "height": 720,
+  "width": 720,
+  "url": "https://fbcdn-photos-a.akamaihd.net/hphotos-ak-ash4/427860_3111729283648_1576860066_32083208_684623008_s.jpg",
+  "provider_name": "facebook",
+  "provider_url": "http://www.facebook.com/photo.php?fbid=3111729283648&set=a.3111729203646.2113918.1576860066&type=1",
+  "author_name": "Beau Gunderson"
+}
+]
+    
+    '''
+
+
+    print photo_json[0]['oembed']
+    print photo_json[0]['at']
+
+    photos_array = []
+    count = 0
+    while count < len(photo_json):
+        oembed_obj = photo_json[count]['oembed']
+        date_obj = photo_json[count]['at']
+        
+        data = {
+            #'date' : date_obj,
+            'source' : oembed_obj['provider_name'],
+            'type' : oembed_obj['type'],
+            'link' : oembed_obj['provider_url'],
+            'image_location' : oembed_obj['url'],
+            'height' : oembed_obj['height'],
+            'width' : oembed_obj['width'],
+        }
+        photos_tuple = [date_obj, data]
+        photos_tuple.sort()
+        photos_array.append(photos_tuple)
+        count += 1
+
+    scrubbed_photo_data = json.JSONEncoder().encode(photos_array)
+    #print "BEGIN JSON"
+    #print scrubbed_data
+
+    ''''
 
     #3 Write profile data to file
-    sfile = open(settings.FILE_WRITE_PATH + 'julia-photos.json', 'w')
-    sfile.write(str(photo_json))
+    sfile = open(settings.FILE_WRITE_PATH + 'julia-photos.json-v2', 'w')
+    sfile.write(photos)
+    #sfile.write(str(photo_json))
+    #sfile.write(json.dumps(photos)
     sfile.close()
 
-    ''' CHECKINS '''
     #2 Make HTTP Re`14yyquests for checkin data
     request_checkins = urllib2.Request(api_request_checkins)
     response_checkins = urllib2.urlopen(request_checkins)
@@ -88,11 +142,10 @@ def singly_authorize(request):
     checkin_json = json.loads(checkins)
 
     #3 Write profile data to file
-    sfile1 = open(settings.FILE_WRITE_PATH + 'julia-checkins.json', 'w')
-    sfile1.write(str(checkin_json))
+    sfile1 = open(settings.FILE_WRITE_PATH + 'julia-checkins-v2.json', 'w')
+    sfile1.write(json.dumps(checkin_json))
     sfile1.close()
 
-    ''' STATUS '''
     #2 Make HTTP Requests for checkin data
     request_statuses = urllib2.Request(api_request_status)
     response_statuses = urllib2.urlopen(request_statuses)
@@ -104,11 +157,10 @@ def singly_authorize(request):
     statuses_json = json.loads(statuses)
 
     #3 Write profile data to file
-    sfile2 = open(settings.FILE_WRITE_PATH + 'julia-statuses.json', 'w')
-    sfile2.write(str(statuses_json))
+    sfile2 = open(settings.FILE_WRITE_PATH + 'julia-statuses-v2.json', 'w')
+    sfile2.write(json.dumps(statuses_json))
     sfile2.close()
 
-    ''' STATUS '''
     #2 Make HTTP Re`14yyquests for checkin data
     request_profiles = urllib2.Request(api_request_profiles)
     response_profiles = urllib2.urlopen(request_profiles)
@@ -120,10 +172,14 @@ def singly_authorize(request):
     checkin_profiles = json.loads(profiles)
 
     #3 Write profile data to file
-    sfile1 = open(settings.FILE_WRITE_PATH + 'julia-profiles.json', 'w')
-    sfile1.write(str(checkin_profiles))
+    sfile1 = open(settings.FILE_WRITE_PATH + 'julia-profiles-v2.json', 'w')
+    sfile1.write(json.dumps(checkin_profiles))
     sfile1.close()
 
-    return HttpResponse('<h1>Page was found</h1>')
-    #return render_to_response('generic/etsy/etsy_start.html',{'sign_in_url': })
+    '''
+
+    #return HttpResponse('<h1>Page was found</h1>')
+    return render_to_response('generic/etsy/etsy_start.html',
+                              {'photo_json' : scrubbed_photo_data,})
+
 
